919924019552aa7eb5f455eeacf78d548d8ed339
smerisanlo <109088416+smerisanlo@users.noreply.github.com>
2022-07-15 11:03:44 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "aws_db_instance" "default" {
+  allocated_storage    = 30
+  engine               = "oracle-se2"
+  engine_version       = "19.0.0.0.ru-2021-10.rur-2021-10.r1"
+  instance_class       = "db.r5.xlarge"
+  name                 = "oracle"
+  username             = "var.secrets.USERNAME"
+  password             = "secrets.RANDOM_PASSWORD"
+  parameter_group_name = "default.oraclel5.7"
+  skip_final_snapshot  = true
+}
+
+ allocated_storage    = 30
+        engine               = "oracle-se2"
+        engine_version       = "19.0.0.0.ru-2021-10.rur-2021-10.r1"
+        instance_class       = "db.r5.xlarge"
+        name                 = "oracle"
+        username             = var.secrets.USERNAME
+        RANDOM_PASSWORD      = secrets.RANDOM_PASSWORD
+        parameter_group_name = "default.oraclel5.7"
+        skip_final_snapshot  = true
