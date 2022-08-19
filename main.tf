258859eb0c567afe2eadbadaebbc823cfe435a90
smerisanlo <109088416+smerisanlo@users.noreply.github.com>
2022-08-19 09:50:22 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,8 @@ resource "aws_db_instance" "default" {
   engine_version       = "19.0.0.0.ru-2021-10.rur-2021-10.r1"
   instance_class       = "db.r5.xlarge"
   name                 = "oracle"
-  username             = "usuario353"
-  key_name              = "pgxinfra"
-  # password             = "secrets.RANDOM_PASSWORD"
+  username             = var.secrets.USERNAME
+  random_password      = var.secrets.RANDOM_PASSWORD
   parameter_group_name = "default.oraclel5.7"
   skip_final_snapshot  = true
 }
